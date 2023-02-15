9f67e069f0a61a8c100a49e8d7ba0063a7bc28fb
Dmitry-Ostashev <dmitry.ostashev@devexpress.com>
2023-02-15 22:19:23 +0400

    add test

--- index.js
+++ index.js
@@ -9,36 +9,45 @@ const util = require('util');
 const { spawn, exec } = require('node:child_process');
 const os   = require('os');
 
-function checkCommandOutput (command) {
+function getCommandOutput (command) {
     return new Promise(resolve => {
         exec(command, (error, stdout, stderr) => {
             console.log(stdout);
 
-            resolve(VM_REGEX.test(stdout));
+            resolve(stdout);
         });
     });
 }
 
 async function isLinuxVM () {
     const LINUX_COMMAND = 'systemd-detect-virt';
+    const output        = await getCommandOutput(LINUX_COMMAND);
 
-    return await checkCommandOutput(LINUX_COMMAND);
+    return VM_REGEX.test(output);
+}
+
+async function isWinVM () {
+    const VM_BIOS              = '0';
+    const BIOS_NUMBER_COMMAND  = 'WMIC BIOS GET SERIALNUMBER';
+    const MODEL_COMMAND        = 'WMIC COMPUTERSYSTEM GET MODEL';
+    const MANUFACTURER_COMMAND = 'WMIC COMPUTERSYSTEM GET MANUFACTURER';
+
+    const biosNumberOutput   = await getCommandOutput(BIOS_NUMBER_COMMAND);
+    const modelOutput        = await getCommandOutput(MODEL_COMMAND);
+    const manufacturerOutput = await getCommandOutput(MANUFACTURER_COMMAND);
+
+    return biosNumberOutput === VM_BIOS || VM_REGEX.test(modelOutput) || VM_REGEX.test(manufacturerOutput);
 }
 
 async function checkIsVM () {
-    let isVM = false;
-
     switch (os.platform()) {
-        case LINUX_PLATFORM: isVM = await isLinuxVM();
-        break;
+        case LINUX_PLATFORM: return await isLinuxVM();
+        case WIN_PLATFORM: return await isWinVM();
         default:
+            return false;
     }
 
     return isVM;
 }
 
-(async () => {
-    const isVM = await checkIsVM();
-
-    console.log(isVM);
-})();
+module.exports = checkIsVM;
